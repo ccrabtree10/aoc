b68e9fc4f6870b0e1a61d0b6ddde958bb12ec997
Chris Crabtree <Chris Crabtree chris.crabtree@matillion.com>
2019-12-21 00:26:19 +0000

    Finish day 1

--- src/main/java/com/ccrabtree/aoc2019/day01/Controller.java
+++ src/main/java/com/ccrabtree/aoc2019/day01/Controller.java
@@ -1,12 +1,34 @@
 package com.ccrabtree.aoc2019.day01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Controller
 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException
+    {
+        int sum1 = 0;
+        int sum2 = 0;
+        String line;
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(ClassLoader.getSystemResourceAsStream("input_01.txt")))) {
+            while ((line = reader.readLine()) != null) {
+                int fuel = calculateFuel(Integer.parseInt(line));
+                sum1 += fuel;
+                sum2 += fuel;
+                while ((fuel = calculateFuel(fuel)) > 0) {
+                    sum2 += fuel;
+                }
+            }
+        }
+        assert sum1 == 3345909;
+        System.out.println("Part 1: " + sum1);
+        assert sum2 == 5015983;
+        System.out.println("Part 2: " + sum2);
     }
 
-    public static int calculateMass(int mass) {
+    public static int calculateFuel(int mass) {
         return Math.floorDiv(mass, 3) - 2;
     }
 }
--- src/test/java/com/ccrabtree/aoc2019/day01/ControllerTest.java
+++ src/test/java/com/ccrabtree/aoc2019/day01/ControllerTest.java
@@ -8,9 +8,9 @@ public class ControllerTest {
 
     @Test
     public void calculateMass_ValidInput_CalculatesCorrectly() {
-        assertEquals(2, Controller.calculateMass(12));
-        assertEquals(2, Controller.calculateMass(14));
-        assertEquals(654, Controller.calculateMass(1969));
-        assertEquals(33583, Controller.calculateMass(100756));
+        assertEquals(2, Controller.calculateFuel(12));
+        assertEquals(2, Controller.calculateFuel(14));
+        assertEquals(654, Controller.calculateFuel(1969));
+        assertEquals(33583, Controller.calculateFuel(100756));
     }
 }
